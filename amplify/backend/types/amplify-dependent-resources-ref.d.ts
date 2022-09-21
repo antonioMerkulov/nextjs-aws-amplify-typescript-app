4c06de372d49b0f4ddcc51d373bbed431e68206b
Anton Merkulov <anton.merkulov@dataart.com>
2022-09-21 13:03:01 +0300

    init backend

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "nextjsawsamplify": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
